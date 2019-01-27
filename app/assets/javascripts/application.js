50dc1f40562be2648d0c4153afb86e7e3a989540
hanna_nyman <hannanyman.sk@gmail.com>
2019-01-27 19:13:51 +0100

    Adds JavaScript functions to open and close navigation Adds styles to navigation Adds preliminary paragraphs instead of links to navigation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,12 +6,34 @@
 // global variables
 let navigationIcon
 let navigation
+let navigationExit
 
 document.addEventListener('DOMContentLoaded', event => {
 	navigationIcon = document.querySelector('#nav')
 	navigation = document.querySelector('#navigation')
+	navigationExit = document.querySelector('#x')
 
 	navigationIcon.addEventListener('click', event => {
-		navigation.style.display = 'block'
+		openNavigation()
+	})
+
+	navigationExit.addEventListener('click', event => {
+		closeNavigation()
 	})
 })
+
+const openNavigation = () => {
+	navigation.style.display = 'block'
+
+	setTimeout(function(){
+		navigation.style.height = '100vh'
+	}, 20)
+}
+
+const closeNavigation = () => {
+	navigation.style.height = '0vh'
+
+	setTimeout(function(){
+		navigation.style.display = 'none'
+	}, 510)
+}
