9b429b0805d3db776e74aa8d228908633f987662
hanna_nyman <hannanyman.sk@gmail.com>
2019-01-27 14:45:10 +0100

    Cleans up code in files

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,10 @@
 FactoryBot.define do
+
   factory :user do
-    name{'Hanna'}
-    email{'hanna@mail.com'}
-    password {'password'}
-    password_confirmation {'password'}
+    name{ 'Hanna' }
+    email{ 'hanna@mail.com' }
+    password { 'password' }
+    password_confirmation { 'password' }
   end
+  
 end
